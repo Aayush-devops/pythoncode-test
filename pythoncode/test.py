99c2b9ba786168d12ff75e364bec01e7b237829e
Ayush sharma <131542303+Aayush-devops@users.noreply.github.com>
2025-06-02 17:18:14 +0530

    Update 12test.py

--- pythoncode/test.py
+++ pythoncode/test.py
@@ -1,9 +1,21 @@
 print("Wassup Buddy this is out last version we all sure about that we are intrigated the jenkins!! v10")
 
-print 10 > 9)           # SyntaxError
-printnkkn(10 == 9)      # NameError (unknown function)
-print(10 < 9)           # This is valid but won't be reached
+print (10 > 9)           
+print(10 == 9)     
+print 10 < 9)           
 
 fruits = ["apple", "banana", "cherry"]
 for x in fruits
-  print(x)      
+  print("x")      
+
+if 10 > 5:
+    print("This is true!")
+    print("I am tab indentation")
+
+print("I have no indentation")
+
+first_name = "Reddy"
+last_name = "Anna"  # assign last name
+
+# print full name
+print(first_name, last_name)
