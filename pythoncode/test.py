c7b466cc698696113c90e692bed4dcc1178d9160
Ayush sharma <131542303+Aayush-devops@users.noreply.github.com>
2025-06-02 15:39:51 +0530

    Update 6 test.py

--- pythoncode/test.py
+++ pythoncode/test.py
@@ -1,1 +1,5 @@
-print Wassup Buddy this is out last version we all sure about that we are intrigated the jenkins!! v10")
+print("Wassup Buddy this is out last version we all sure about that we are intrigated the jenkins!! v10")
+
+print 10 > 9)
+print 10 == 9)
+print(10 < 9)
