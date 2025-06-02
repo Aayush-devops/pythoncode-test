08b4b75a19c0bfd10f333e2b7062ed1515754807
Ayush sharma <131542303+Aayush-devops@users.noreply.github.com>
2025-06-02 15:43:43 +0530

    Update10 test.py

--- pythoncode/test.py
+++ pythoncode/test.py
@@ -1,5 +1,9 @@
 print("Wassup Buddy this is out last version we all sure about that we are intrigated the jenkins!! v10")
 
 print 10 > 9)
-print 10 == 9)
+printnkkn 10 == 9)
 print(10 < 9)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
