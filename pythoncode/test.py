724750ce81915d3dd1d2257090f5b475fae343f4
Ayush sharma <131542303+Aayush-devops@users.noreply.github.com>
2025-06-02 16:13:12 +0530

    Update11 test.py

--- pythoncode/test.py
+++ pythoncode/test.py
@@ -1,9 +1,9 @@
 print("Wassup Buddy this is out last version we all sure about that we are intrigated the jenkins!! v10")
 
-print 10 > 9)
-printnkkn 10 == 9)
-print(10 < 9)
+print 10 > 9)           # SyntaxError
+printnkkn(10 == 9)      # NameError (unknown function)
+print(10 < 9)           # This is valid but won't be reached
 
 fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-  print(x)
+for x in fruits
+  print(x)      
