49743255605adc94ed816fe06f448997169871fb
Ayush sharma <131542303+Aayush-devops@users.noreply.github.com>
2025-06-02 17:23:17 +0530

    Update new test.py

--- pythoncode/test.py
+++ pythoncode/test.py
@@ -4,16 +4,6 @@ print (10 > 9)
 print(10 == 9)     
 print (10 < 9)           
 
-fruits = ["apple", "banana", "cherry"]
-for x in fruits
-  print("x")      
-
-if 10 > 5:
-    print("This is true!")
-    print("I am tab indentation")
-
-print("I have no indentation")
-
 first_name = "Reddy"
 last_name = "Anna"  # assign last name
 
