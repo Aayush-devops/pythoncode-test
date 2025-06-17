2fa0660a1ab209ce3b1b99ff0ad268167bb2af23
Ayush sharma <131542303+Aayush-devops@users.noreply.github.com>
2025-06-18 02:00:13 +0530

    Update9 test.py

--- pythoncode/test.py
+++ pythoncode/test.py
@@ -1,7 +1,7 @@
 print("Wassup Buddy this is out last version we all sure about that we are intrigated the jenkins!! v10")
 
 print 10 9)           
-print(10 == 9)     
+print 10 == 9)     
 print (10 < 9)           
 
 first_name = "Reddy"
