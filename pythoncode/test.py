8fd44c6a95c05008199640022a3a162931df7844
Ayush sharma <131542303+Aayush-devops@users.noreply.github.com>
2025-06-18 01:58:07 +0530

    Update8 test.py

--- pythoncode/test.py
+++ pythoncode/test.py
@@ -1,6 +1,6 @@
 print("Wassup Buddy this is out last version we all sure about that we are intrigated the jenkins!! v10")
 
-print (10 > 9)           
+print 10 9)           
 print(10 == 9)     
 print (10 < 9)           
 
